e58b5e91e174cd9446fdf3860723c016d9c8322f
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-08 00:59:26 +0200

    imported dependencies to app

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const challengeRouter = require('./routes/challenge');
+
+const app = express();
 
 const options = {
   useUnifiedTopology: true,
@@ -10,6 +22,7 @@ mongoose
   .then(() => console.log('Connected to DB'))
   .catch((err) => console.error('Could not connect to DB', err));
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,5 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/challenge', challengeRouter);
 
 module.exports = app;
