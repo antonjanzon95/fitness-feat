a7cfc8f951dafdab0887b1f1dd6235d19c88b9bb
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-07 12:50:39 +0200

    added home,about,contact routes

---
+++ frontend/src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  links = [
+    { name: 'Home', path: '' },
+    { name: 'About', path: 'about' },
+    { name: 'Contact', path: 'contact' },
+  ];
+}
