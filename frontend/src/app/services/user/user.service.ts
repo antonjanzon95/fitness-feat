aa5db8e0e17f3699d75bf7125edd4a894925bd61
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-10 00:32:04 +0200

    added user service for fetching users

---
+++ frontend/src/app/services/user/user.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { Observable, from, switchMap } from 'rxjs';
+import { getAccessTokenHeaders } from 'src/app/helpers/auth0.helper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient, private auth: AuthService) {}
+
+  getUsers(): Observable<any> {
+    return getAccessTokenHeaders(this.auth).pipe(
+      switchMap((headers) => {
+        return this.http.get('http://localhost:3000/users', { headers });
+      })
+    );
+  }
+
+  addUser(name: string, email: string, auth0Id: string): Observable<any> {
+    return getAccessTokenHeaders(this.auth).pipe(
+      switchMap((headers) => {
+        return this.http.post(
+          'http://localhost:3000/users/login',
+          { name: name, email: email, auth0Id: auth0Id },
+          { headers }
+        );
+      })
+    );
+  }
+}
