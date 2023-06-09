e4e26b4c7f20e4af07a602a984e150430e61c4aa
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-10 00:31:49 +0200

    added workout service for fetching workouts

---
+++ frontend/src/app/services/workout/workout.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { Observable, from, switchMap } from 'rxjs';
+import { getAccessTokenHeaders } from 'src/app/helpers/auth0.helper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WorkoutService {
+  constructor(private http: HttpClient, private auth: AuthService) {}
+
+  getWorkouts(): Observable<any> {
+    return getAccessTokenHeaders(this.auth).pipe(
+      switchMap((headers) => {
+        return this.http.get('http://localhost:3000/workouts', { headers });
+      })
+    );
+  }
+
+  addWorkout(
+    type: string,
+    duration: number,
+    intensity: string
+  ): Observable<any> {
+    return getAccessTokenHeaders(this.auth).pipe(
+      switchMap((headers) => {
+        return this.http.post(
+          'http://localhost:3000/workouts/add',
+          { type: type, duration: duration, intensity: intensity },
+          { headers }
+        );
+      })
+    );
+  }
+}
