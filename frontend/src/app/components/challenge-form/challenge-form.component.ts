db96231f84b73a0c0ac68641982d432f355002f7
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-12 20:58:37 +0200

    added form for adding challenges

---
+++ frontend/src/app/components/challenge-form/challenge-form.component.ts
@@ -0,0 +1,66 @@
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ChallengeService } from 'src/app/services/challenge/challenge.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-challenge-form',
+  templateUrl: './challenge-form.component.html',
+  styleUrls: ['./challenge-form.component.css'],
+})
+export class ChallengeFormComponent {
+  challengeForm: FormGroup;
+  successMsg: string = 'You have successfully added your challenge!';
+  failedMsg: string = 'Failed to add challenge, please try again.';
+
+  @ViewChild('form') form: any;
+
+  constructor(
+    private formbuilder: FormBuilder,
+    private challengeService: ChallengeService,
+    private snackBar: MatSnackBar
+  ) {
+    this.challengeForm = this.formbuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      startDate: [new Date(), Validators.required],
+      endDate: [new Date(), Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.challengeForm.invalid) return;
+
+    const challengeData = {
+      name: this.challengeForm.value.name,
+      description: this.challengeForm.value.description,
+      startDate: this.challengeForm.value.startDate,
+      endDate: this.challengeForm.value.endDate,
+    };
+
+    console.log(challengeData);
+
+    this.challengeService
+      .addChallenge(
+        challengeData.name,
+        challengeData.description,
+        challengeData.startDate,
+        challengeData.endDate
+      )
+      .subscribe({
+        next: (val) => {
+          console.log(val);
+          this.form.resetForm();
+          this.snackBar.open(this.successMsg, 'Close', {
+            duration: 3000,
+          });
+        },
+        error: (err) => {
+          console.error(err);
+          this.snackBar.open(this.failedMsg, 'Close', {
+            duration: 3000,
+          });
+        },
+      });
+  }
+}
