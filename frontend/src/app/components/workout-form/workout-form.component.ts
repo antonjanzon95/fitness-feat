8f9d4190374c0d4505b01cd2ce4b875a5f97e3cc
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-12 20:58:07 +0200

    added snack bar to workout-form

--- frontend/src/app/components/workout-form/workout-form.component.ts
+++ frontend/src/app/components/workout-form/workout-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { WorkoutService } from 'src/app/services/workout/workout.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-workout-form',
@@ -9,10 +10,13 @@ import { WorkoutService } from 'src/app/services/workout/workout.service';
 })
 export class WorkoutFormComponent {
   workoutForm: FormGroup;
+  successMsg: string = 'You have successfully added your workout!';
+  failedMsg: string = 'Failed to add workout, please try again.';
 
   constructor(
     private formbuilder: FormBuilder,
-    private workoutService: WorkoutService
+    private workoutService: WorkoutService,
+    private snackBar: MatSnackBar
   ) {
     this.workoutForm = this.formbuilder.group({
       type: ['', Validators.required],
@@ -35,9 +39,16 @@ export class WorkoutFormComponent {
       .subscribe({
         next: (val) => {
           console.log(val);
+          this.workoutForm.reset();
+          this.snackBar.open(this.successMsg, 'Close', {
+            duration: 3000,
+          });
         },
         error: (err) => {
           console.error(err);
+          this.snackBar.open(this.failedMsg, 'Close', {
+            duration: 3000,
+          });
         },
       });
   }
