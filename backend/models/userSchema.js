471245dfbfc346b88f26804551a2fcd4e53e85d9
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-13 17:08:22 +0200

    updated user schema

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   auth0Id: { type: String, required: true, unique: true },
+  id: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   name: String,
   bio: String,
+  picture: String,
   goals: String,
+  startingWeight: Number,
+  currentWeight: Number,
+  height: Number,
+  totalWorkoutTime: Number,
   workoutHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Workout' }],
   challengesJoined: [
     { type: mongoose.Schema.Types.ObjectId, ref: 'Challenge' },
