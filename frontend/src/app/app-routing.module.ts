b83f21523182975040f7f29a66b8d01a131576ef
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-13 17:13:00 +0200

    added user profile route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './views/about/about.component';
 import { ContactComponent } from './views/contact/contact.component';
 import { HomeComponent } from './views/home/home.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'dashboard', component: UserDashboardComponent },
+  { path: 'user-profile/:id', component: UserProfileComponent },
 ];
 
 @NgModule({
