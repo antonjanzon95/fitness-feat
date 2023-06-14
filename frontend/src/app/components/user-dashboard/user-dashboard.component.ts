9c30b63c483efc263941de513313d4a99694a8c3
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-14 22:27:02 +0200

    added user dashboard component

--- frontend/src/app/components/user-dashboard/user-dashboard.component.ts
+++ frontend/src/app/components/user-dashboard/user-dashboard.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/models/IUser';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
-  styleUrls: ['./user-dashboard.component.css']
+  styleUrls: ['./user-dashboard.component.css'],
 })
-export class UserDashboardComponent {
+export class UserDashboardComponent implements OnInit {
+  user: IUser | undefined;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUser().subscribe({
+      next: (val) => {
+        console.log(val);
+        this.user = val;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 }
