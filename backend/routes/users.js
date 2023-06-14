54b2148b5a5b28bee26565144a6afe3c8d880355
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-14 22:29:30 +0200

    added user controller

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,12 +2,30 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/userSchema');
 const { validateAccessToken } = require('../middleware/auth0.middleware');
+const { attachUserToRequest } = require('../middleware/attach-user.middleware');
 const { v4: uuidv4 } = require('uuid');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get(
+  '/user',
+  validateAccessToken,
+  attachUserToRequest,
+  async function (req, res, next) {
+    const { dbUser } = req.user;
+
+    try {
+      const user = await User.findById(dbUser._id).populate();
+
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(500).json({ message: 'Error finding user: ', error });
+    }
+  }
+);
 
 router.post('/login', validateAccessToken, async (req, res, next) => {
   const { auth0Id, name, email, picture } = req.body;
