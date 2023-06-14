30594616f24de9f39fb73965a4443d81028719c0
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-14 22:27:53 +0200

    split challenge form from challenge comp

--- frontend/src/app/components/challenge/challenge.component.ts
+++ frontend/src/app/components/challenge/challenge.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { IChallenge } from 'src/app/models/IChallenge';
 
 @Component({
@@ -6,13 +13,30 @@ import { IChallenge } from 'src/app/models/IChallenge';
   templateUrl: './challenge.component.html',
   styleUrls: ['./challenge.component.css'],
 })
-export class ChallengeComponent {
+export class ChallengeComponent implements OnChanges {
   @Input() currentChallenge: IChallenge | undefined;
   @Output() resetViewEmit: EventEmitter<null> = new EventEmitter<null>();
+  daysRemaining: number | undefined;
 
   constructor() {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['currentChallenge']) {
+      if (this.currentChallenge) {
+        this.daysRemaining = this.calculateDaysRemaining(
+          new Date(this.currentChallenge?.endDate)
+        );
+      }
+    }
+  }
+
   resetView() {
     this.resetViewEmit.emit();
+  }
+
+  calculateDaysRemaining(endDate: Date): number {
+    return Math.floor(
+      Math.abs(endDate.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24)
+    );
   }
 }
