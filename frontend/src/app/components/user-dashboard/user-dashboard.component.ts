93c52e8ff530333a1d05570065dcde605baffa16
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-30 15:08:47 +0200

    added snackbar

--- frontend/src/app/components/user-dashboard/user-dashboard.component.ts
+++ frontend/src/app/components/user-dashboard/user-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
-import { IUser } from 'src/app/models/IUser';
+import { IUser, IUserInfo } from 'src/app/models/IUser';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
@@ -11,20 +12,31 @@ import { UserService } from 'src/app/services/user/user.service';
 export class UserDashboardComponent implements OnInit {
   user$: Observable<IUser | null> | undefined;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.user$ = this.userService.user$;
   }
 
-  updateUser() {
-    this.userService.getUser().subscribe({
+  updateUserImage(url: string) {
+    this.userService.updateUserImage(url).subscribe({
       next: () => {
-        console.log('test');
+        console.log('url: ', url);
+        this.snackBar.open('Successfully changed svatar!', 'Close', {
+          duration: 3000,
+        });
       },
       error: (err) => {
         console.error(err);
+        this.snackBar.open('Failed to change avatar. Try again.', 'Close', {
+          duration: 3000,
+        });
       },
     });
   }
+
+  updateUserInfo(userData: IUserInfo) {}
 }
