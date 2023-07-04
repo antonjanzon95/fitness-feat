8cb0c8b2c8cdcef2bf770234bdc6dd9675ee9e36
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-04 23:08:22 +0200

    added workout reducer

---
+++ frontend/src/app/state/workouts/workouts.reducer.ts
@@ -0,0 +1,34 @@
+import { createReducer, on } from '@ngrx/store';
+import { IWorkout } from 'src/app/models/IWorkout';
+import { WorkoutActions } from './workouts.actions';
+
+export interface WorkoutState {
+  workouts: IWorkout[];
+  error: string | null;
+  status: 'pending' | 'loading' | 'error' | 'success';
+}
+
+export const initialState: WorkoutState = {
+  workouts: [],
+  error: null,
+  status: 'pending',
+};
+
+export const workoutsReducer = createReducer(
+  initialState,
+  on(WorkoutActions.getWorkouts, (state) => ({
+    ...state,
+    status: 'loading' as 'loading',
+  })),
+  on(WorkoutActions.getWorkoutsFailure, (state, { error }) => ({
+    ...state,
+    error: error,
+    status: 'error' as 'error',
+  })),
+  on(WorkoutActions.getWorkoutsSuccess, (state, { workouts }) => ({
+    ...state,
+    workouts: workouts,
+    error: null,
+    status: 'success' as 'success',
+  }))
+);
