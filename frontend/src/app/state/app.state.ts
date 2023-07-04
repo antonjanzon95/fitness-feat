485eb0579801ddf20df283bfcf765b5ed5c6977a
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-04 23:08:42 +0200

    updated app state

--- frontend/src/app/state/app.state.ts
+++ frontend/src/app/state/app.state.ts
@@ -1,5 +1,7 @@
 import { UserState } from '../state/user/user.reducer';
+import { WorkoutState } from './workouts/workouts.reducer';
 
 export interface IAppState {
   user: UserState;
+  workouts: WorkoutState;
 }
