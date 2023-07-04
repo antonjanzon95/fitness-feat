f6910b49c88ce5a58a797cff1549b82278512590
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-04 19:41:13 +0200

    added user observable

--- frontend/src/app/components/user-profile/user-profile.component.ts
+++ frontend/src/app/components/user-profile/user-profile.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { IUser } from 'src/app/models/IUser';
+import { Store } from '@ngrx/store';
 import { UserService } from 'src/app/services/user/user.service';
+import { IAppState } from 'src/app/state/app.state';
+import { selectCurrentUser } from 'src/app/state/user/user.selector';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
 })
 export class UserProfileComponent implements OnInit {
-  user$: Observable<IUser | null> | undefined;
-  constructor(public userService: UserService) {}
+  user$ = this.store.select(selectCurrentUser);
+  constructor(
+    private store: Store<IAppState>,
+    public userService: UserService
+  ) {}
 
-  ngOnInit(): void {
-    this.user$ = this.userService.user$;
-  }
+  ngOnInit(): void {}
 }
