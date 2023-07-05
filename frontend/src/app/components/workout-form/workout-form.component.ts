9dafebbd9a835f944ddbc078fa397cbb9f89aca4
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-06 01:33:30 +0200

    added modal and ngrx to workouts component

--- frontend/src/app/components/workout-form/workout-form.component.ts
+++ frontend/src/app/components/workout-form/workout-form.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, Optional } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { WorkoutService } from 'src/app/services/workout/workout.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
+import { WorkoutActions } from 'src/app/state/workouts/workouts.actions';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-workout-form',
@@ -10,19 +11,21 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class WorkoutFormComponent {
   workoutForm: FormGroup;
-  successMsg: string = 'You have successfully added your workout!';
-  failedMsg: string = 'Failed to add workout, please try again.';
 
   constructor(
     private formbuilder: FormBuilder,
-    private workoutService: WorkoutService,
-    private snackBar: MatSnackBar
+    @Optional() public dialogRef: MatDialogRef<WorkoutFormComponent>,
+    private store: Store
   ) {
     this.workoutForm = this.formbuilder.group({
       type: ['', Validators.required],
       duration: ['', Validators.required],
       intensity: ['', Validators.required],
     });
+  }
+
+  onNoClick() {
+    this.dialogRef.close();
   }
 
   onSubmit() {
@@ -34,22 +37,7 @@ export class WorkoutFormComponent {
       intensity: this.workoutForm.value.intensity,
     };
 
-    this.workoutService
-      .addWorkout(workoutData.type, workoutData.duration, workoutData.intensity)
-      .subscribe({
-        next: (val) => {
-          console.log(val);
-          this.workoutForm.reset();
-          this.snackBar.open(this.successMsg, 'Close', {
-            duration: 3000,
-          });
-        },
-        error: (err) => {
-          console.error(err);
-          this.snackBar.open(this.failedMsg, 'Close', {
-            duration: 3000,
-          });
-        },
-      });
+    this.store.dispatch(WorkoutActions.addWorkout({ workout: workoutData }));
+    this.workoutForm.reset();
   }
 }
--- frontend/src/app/components/workouts/workouts/workouts.component.ts
+++ frontend/src/app/components/workouts/workouts/workouts.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
-import { WorkoutService } from 'src/app/services/workout/workout.service';
 import { IAppState } from 'src/app/state/app.state';
 import { WorkoutActions } from 'src/app/state/workouts/workouts.actions';
 import { selectAllWorkouts } from 'src/app/state/workouts/workouts.selectors';
+import { WorkoutFormComponent } from '../../workout-form/workout-form.component';
+import { Subscription } from 'rxjs';
+import { Actions, ofType } from '@ngrx/effects';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-workouts',
@@ -12,10 +16,49 @@ import { selectAllWorkouts } from 'src/app/state/workouts/workouts.selectors';
 })
 export class WorkoutsComponent implements OnInit {
   workouts$ = this.store.select(selectAllWorkouts);
+  displayedColumns: string[] = ['type', 'duration', 'intensity', 'date'];
+  addWorkoutSuccessSub: Subscription | undefined;
+  addWorkoutFailureSub: Subscription | undefined;
+  successMsg: string = 'You have successfully added your workout!';
+  failedMsg: string = 'Failed to add workout, please try again.';
 
-  constructor(private store: Store<IAppState>) {}
+  constructor(
+    private store: Store<IAppState>,
+    public dialog: MatDialog,
+    private actions$: Actions,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.store.dispatch(WorkoutActions.getWorkouts());
+
+    this.addWorkoutSuccessSub = this.actions$
+      .pipe(ofType(WorkoutActions.addWorkoutSuccess))
+      .subscribe(() => {
+        this.snackBar.open(this.successMsg, 'Close', {
+          duration: 3000,
+        });
+      });
+
+    this.addWorkoutFailureSub = this.actions$
+      .pipe(ofType(WorkoutActions.addWorkoutFailure))
+      .subscribe(() => {
+        this.snackBar.open(this.failedMsg, 'Close', {
+          duration: 3000,
+        });
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.addWorkoutFailureSub?.unsubscribe();
+    this.addWorkoutSuccessSub?.unsubscribe();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(WorkoutFormComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
   }
 }
