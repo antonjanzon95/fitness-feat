d775e8dfd08b2e3ff6a2a9ac50f0f0c1e6152d41
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-06 01:31:36 +0200

    updated workout interface

--- frontend/src/app/models/IWorkout.ts
+++ frontend/src/app/models/IWorkout.ts
@@ -1,8 +1,11 @@
 import { IUser } from './IUser';
 
-export interface IWorkout {
+export interface IWorkout extends IWorkoutData {
   user: IUser;
   date: Date;
+}
+
+export interface IWorkoutData {
   type: string;
   duration: number;
   intensity: string;
