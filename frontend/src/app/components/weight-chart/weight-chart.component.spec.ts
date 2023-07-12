51a8b3191c1155e3daaa5ea6628b2410d1f946a5
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-13 00:34:40 +0200

    added weight chart component

---
+++ frontend/src/app/components/weight-chart/weight-chart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WeightChartComponent } from './weight-chart.component';
+
+describe('WeightChartComponent', () => {
+  let component: WeightChartComponent;
+  let fixture: ComponentFixture<WeightChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WeightChartComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WeightChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
