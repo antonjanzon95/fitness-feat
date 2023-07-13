62f9c547b13a4512c702b81bbac79e57d9487b31
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-14 00:53:21 +0200

    added weight chart

--- frontend/src/app/components/weight-chart/weight-chart.component.ts
+++ frontend/src/app/components/weight-chart/weight-chart.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { IAppState } from 'src/app/state/app.state';
+import { UserActions } from 'src/app/state/user/user.actions';
+import {
+  selectCurrentUser,
+  selectWeightEntries,
+} from 'src/app/state/user/user.selector';
+import { Chart, ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-weight-chart',
   templateUrl: './weight-chart.component.html',
-  styleUrls: ['./weight-chart.component.css']
+  styleUrls: ['./weight-chart.component.css'],
 })
-export class WeightChartComponent {
+export class WeightChartComponent implements OnInit, OnDestroy {
+  weightEntries$ = this.store.select(selectWeightEntries);
+  user$ = this.store.select(selectCurrentUser);
+  weightSubscription: Subscription | undefined;
+  userSubscription: Subscription | undefined;
+  startingWeight: number | undefined;
 
+  nameLabel: string = '';
+  chartData: number[] = [];
+  chartLabels: string[] = [];
+
+  public lineChartType: ChartType = 'line';
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [],
+  };
+
+  public lineChartOptions: ChartConfiguration['options'] = {};
+
+  constructor(private store: Store<IAppState>) {
+    store.dispatch(UserActions.getWeightEntries());
+  }
+
+  private updateChartLabel() {
+    this.lineChartData.datasets[0].label = this.nameLabel;
+  }
+
+  ngOnInit() {
+    this.weightSubscription = this.weightEntries$.subscribe((entries) => {
+      if (!entries) return;
+      this.startingWeight = entries[0].weight;
+      if (!this.startingWeight) return;
+      this.chartData = entries.map((entry, index) =>
+        index === 0 ? 0 : entry.weight - this.startingWeight!
+      );
+      this.chartLabels = entries.map((entry) => entry.timestamp.toString());
+
+      this.lineChartOptions = {
+        scales: {
+          y: {
+            suggestedMin: -10,
+            suggestedMax: 10,
+          },
+        },
+        plugins: {
+          legend: { display: true },
+        },
+      };
+
+      this.lineChartData = {
+        datasets: [
+          {
+            data: this.chartData,
+            label: this.nameLabel,
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+            fill: 'origin',
+          },
+        ],
+        labels: this.chartLabels,
+      };
+    });
+
+    this.userSubscription = this.user$.subscribe((user) => {
+      if (!user) return;
+      this.nameLabel = user.name;
+      this.updateChartLabel();
+    });
+  }
+
+  ngOnDestroy() {
+    this.weightSubscription?.unsubscribe();
+    this.userSubscription?.unsubscribe();
+  }
 }
