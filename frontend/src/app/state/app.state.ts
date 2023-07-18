0f338f58c6730b03d02b28dfb58b96972e00d548
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-18 19:05:57 +0200

    added challenges state to app state

--- frontend/src/app/state/app.state.ts
+++ frontend/src/app/state/app.state.ts
@@ -1,7 +1,9 @@
 import { UserState } from '../state/user/user.reducer';
+import { ChallengesState } from './challenges/challenges.reducer';
 import { WorkoutState } from './workouts/workouts.reducer';
 
 export interface IAppState {
   user: UserState;
   workouts: WorkoutState;
+  challenges: ChallengesState;
 }
