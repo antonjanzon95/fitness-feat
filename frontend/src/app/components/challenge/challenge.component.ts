073e0e4afc36c7fe191e8513a7b3785822a226ba
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-18 19:13:15 +0200

    refactored to use ngrx

--- frontend/src/app/components/challenge/challenge.component.ts
+++ frontend/src/app/components/challenge/challenge.component.ts
@@ -1,27 +1,36 @@
 import {
   Component,
-  EventEmitter,
-  Input,
   OnChanges,
-  Output,
+  OnDestroy,
+  OnInit,
   SimpleChanges,
 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { IChallenge } from 'src/app/models/IChallenge';
+import { IAppState } from 'src/app/state/app.state';
+import { selectCurrentChallenge } from 'src/app/state/challenges/challenger.selector';
+import { ChallengeActions } from 'src/app/state/challenges/challenges.actions';
+import { WeightEntryFormComponent } from '../weight-entry-form/weight-entry-form.component';
+import { selectWeightEntryError } from 'src/app/state/user/user.selector';
 
 @Component({
   selector: 'app-challenge',
   templateUrl: './challenge.component.html',
   styleUrls: ['./challenge.component.css'],
 })
-export class ChallengeComponent implements OnChanges {
-  @Input() currentChallenge: IChallenge | undefined;
-  @Output() resetViewEmit: EventEmitter<null> = new EventEmitter<null>();
+export class ChallengeComponent implements OnChanges, OnInit, OnDestroy {
+  currentChallenge$ = this.store.select(selectCurrentChallenge);
+  weightEntryError$ = this.store.select(selectWeightEntryError);
+  currentChallenge: IChallenge | undefined;
+  subscription: Subscription | undefined;
   daysRemaining: number | undefined;
 
-  constructor() {}
+  constructor(private store: Store<IAppState>, private dialog: MatDialog) {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['currentChallenge']) {
+    if (changes['currentChallenge$']) {
       if (this.currentChallenge) {
         this.daysRemaining = this.calculateDaysRemaining(
           new Date(this.currentChallenge?.endDate)
@@ -30,8 +39,28 @@ export class ChallengeComponent implements OnChanges {
     }
   }
 
+  ngOnInit(): void {
+    this.subscription = this.currentChallenge$.subscribe((result) => {
+      if (result !== null) {
+        this.currentChallenge = result;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+
   resetView() {
-    this.resetViewEmit.emit();
+    this.store.dispatch(ChallengeActions.resetCurrentChallenge());
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(WeightEntryFormComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
   }
 
   calculateDaysRemaining(endDate: Date): number {
