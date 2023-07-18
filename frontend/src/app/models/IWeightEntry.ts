eac54c35ca56fd00731f13bd2d373df603a1bb31
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-18 19:07:59 +0200

    updated weight entry interface

--- frontend/src/app/models/IWeightEntry.ts
+++ frontend/src/app/models/IWeightEntry.ts
@@ -1,7 +1,9 @@
+import { IChallenge } from './IChallenge';
 import { IUser } from './IUser';
 
 export interface IWeightEntry {
   user: IUser;
   weight: number;
   timestamp: Date;
+  challenge?: IChallenge;
 }
