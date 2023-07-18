8bad9e9bf205345a708cb6cbcb7c53abf196fa64
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-07-18 19:08:41 +0200

    added flexibility to add challenge weight entry

--- frontend/src/app/components/weight-entry-form/weight-entry-form.component.ts
+++ frontend/src/app/components/weight-entry-form/weight-entry-form.component.ts
@@ -1,8 +1,11 @@
-import { Component, Optional } from '@angular/core';
+import { Component, OnDestroy, OnInit, Optional } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { IAppState } from 'src/app/state/app.state';
+import { selectCurrentChallenge } from 'src/app/state/challenges/challenger.selector';
+import { ChallengeActions } from 'src/app/state/challenges/challenges.actions';
 import { UserActions } from 'src/app/state/user/user.actions';
 
 @Component({
@@ -10,8 +13,11 @@ import { UserActions } from 'src/app/state/user/user.actions';
   templateUrl: './weight-entry-form.component.html',
   styleUrls: ['./weight-entry-form.component.css'],
 })
-export class WeightEntryFormComponent {
+export class WeightEntryFormComponent implements OnInit, OnDestroy {
   weightEntryForm: FormGroup;
+  currentChallenge$ = this.store.select(selectCurrentChallenge);
+  challengeSubscription: Subscription | undefined;
+  challengeId: string | undefined;
 
   constructor(
     private store: Store<IAppState>,
@@ -23,9 +29,28 @@ export class WeightEntryFormComponent {
     });
   }
 
+  ngOnInit(): void {
+    this.challengeSubscription = this.currentChallenge$.subscribe((res) => {
+      this.challengeId = res?._id;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.challengeSubscription?.unsubscribe();
+  }
+
   onSubmit() {
     if (this.weightEntryForm.invalid) return;
 
+    if (this.currentChallenge$) {
+      if (!this.challengeId) return;
+      this.store.dispatch(
+        UserActions.newChallengeWeightEntry({
+          weight: this.weightEntryForm.value.weight,
+          challengeId: this.challengeId,
+        })
+      );
+    }
     this.store.dispatch(
       UserActions.newWeightEntry({
         weight: this.weightEntryForm.value.weight,
